5910253cf3792f2f719d7f03a7303b3da90a56ff
AlanUzielo <lalanuziel@gmail.com>
2023-11-05 22:29:00 -0600

    Descargué bootstrap, hice servicio para conectar a base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VistasComponent } from './vistas/vistas.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BajasComponent } from './bajas/bajas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VistasComponent
+    VistasComponent,
+    ClienteComponent,
+    BajasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbclient.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbclientService {
+
+  constructor(private http:HttpClient) { }
+
+  alta(datos:any){
+    return this.http.post('http://localhost:3000/alta', datos);
+  }
+
+  baja(datos:any){
+    return this.http.post('http://localhost:3000/baja', datos);
+  }
+
+  cambio(datos:any){
+    return this.http.post('http://localhost:3000/cambio', datos);
+  }
+
+  consulta(datos:any){
+    return this.http.post('http://localhost:3000/consulta', datos);
+  }
+}
