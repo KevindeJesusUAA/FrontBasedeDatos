5613b562384243b53e2586033c037f032577508f
NeriU <118403833+NeriU@users.noreply.github.com>
2023-11-05 21:04:14 -0600

    intento 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProovedorComponent } from './proovedor/proovedor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProovedorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/proovedor/proovedor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proovedor',
+  templateUrl: './proovedor.component.html',
+  styleUrls: ['./proovedor.component.css']
+})
+export class ProovedorComponent {
+
+}
