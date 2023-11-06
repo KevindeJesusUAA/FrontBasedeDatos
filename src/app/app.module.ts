8c7dfc3cf75e381a492d0c20eb27a7eb5e7e05ea
KevindeJesusUAA <118912199+KevindeJesusUAA@users.noreply.github.com>
2023-11-05 20:13:59 -0600

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VistasComponent } from './vistas/vistas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistasComponent
   ],
   imports: [
     BrowserModule,
