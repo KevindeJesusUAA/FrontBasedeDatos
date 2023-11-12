ee86d28a9b5a4c6875897a16293fceb1bcbfc261
AlanUzielo <lalanuziel@gmail.com>
2023-11-12 01:49:45 -0600

    Funcion de Cambios y valide todos los input para ingresar datos coherentes

--- src/app/cambiosform/cambiosform.component.ts
+++ src/app/cambiosform/cambiosform.component.ts
@@ -13,7 +13,6 @@ export class CambiosformComponent implements OnInit {
   propiedades:any;
   resultado:any;
 
-
   constructor(private servicio:DbclientService) { }
 
   ngOnInit() {
@@ -21,15 +20,84 @@ export class CambiosformComponent implements OnInit {
     this.tabla = this.registro.tabla;
     this.registro = this.registro.registro;
     this.propiedades = Object.keys(this.registro);
+    
+  }
+
+  isNumber(propiedad:string):boolean{
+    return propiedad.includes("edad") || propiedad.includes("costo") || propiedad.includes("existencia")
+    || propiedad.includes("sueldo")
   }
 
   cambios(){
-    // llamar a la api para guardar los cambios
-  /*this.servicio.cambio(this.tabla).then((data) => {
-      this.resultado = data;
-    })
-    .catch((err) => {
-      console.log(err); 
-    });*/
+    if(this.validaTelefono() && this.validaNombre()
+    && this.validaCosto() && this.validaTurno()){
+      
+      const obj = {
+        tabla: this.tabla,
+        registro: this.registro
+      }
+
+      this.servicio.cambio(obj).then((data) => {
+        this.resultado = data;
+      })
+      .catch((err) => {
+        console.log(err); 
+      });
+
+    }else{
+      console.log("NO pasa a la BD!!");
+    }
+  }
+
+  validaTelefono():boolean{
+    for(let prop in this.registro){
+      if(prop.includes("telefono")){
+        if(/^[0-9]*$/.test(this.registro[prop])){
+          return true;
+        }else{
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  validaNombre():boolean{
+    for(let prop in this.registro){
+      if(prop.includes("nombre") || prop.includes("marca")){
+        if(/^[a-zA-Z\s]*$/.test(this.registro[prop])){
+          return true;
+        }else{
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  validaCosto():boolean{
+    for(let prop in this.registro){
+      if(prop.includes("costo")){
+        if(/^[0-9]*$/.test(this.registro[prop])){
+          return true;
+        }else{
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  validaTurno():boolean{
+    for(let prop in this.registro){
+      if(prop.includes("turno")){
+        if(/^(matutino|vespertino)$/i.test(this.registro[prop])){
+          return true;
+        }else{
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
--- src/app/dbclient.service.ts
+++ src/app/dbclient.service.ts
@@ -34,7 +34,25 @@ export class DbclientService {
   }
 
   cambio(datos:any){
-    datos = JSON.stringify(datos);
+    // Recupera npmbre de la tabla
+    let tabla = datos.tabla;
+    // Elimina el nombre de la tabla del objeto
+    delete datos.tabla;
+    datos = datos.registro;
+    
+    switch(tabla){
+      case 'cliente':
+        return this.http.put('http://localhost:3000/cliente_edit', datos).toPromise();
+      case 'producto':
+        return this.http.put('http://localhost:3000/producto_edit', datos).toPromise();
+      case 'proveedor':
+        return this.http.put('http://localhost:3000/proveedor_edit', datos).toPromise();
+      case 'empleado':
+        return this.http.put('http://localhost:3000/empleado_edit', datos).toPromise();
+      default:
+        return this.http.put('http://localhost:3000/doctor_edit', datos).toPromise();
+    }
+
   }
 
   consulta(datos:any){
@@ -91,15 +109,14 @@ export class DbclientService {
     switch(tabla){
       case 'producto':
         return this.http.get('http://localhost:3000/producto_all').toPromise();
-              case 'cliente':
-              return this.http.get('http://localhost:3000/cliente_all').toPromise();
-              case 'proveedor':
-              return this.http.get('http://localhost:3000/proveedor_all').toPromise();
-              case 'empleado':
-              return this.http.get('http://localhost:3000/empleado_all').toPromise();
-              case 'doctor':
-              return this.http.get('http://localhost:3000/doctor_all').toPromise();
-                 
+      case 'cliente':
+        return this.http.get('http://localhost:3000/cliente_all').toPromise();
+      case 'proveedor':
+        return this.http.get('http://localhost:3000/proveedor_all').toPromise();
+      case 'empleado':
+        return this.http.get('http://localhost:3000/empleado_all').toPromise();
+      case 'doctor':
+        return this.http.get('http://localhost:3000/doctor_all').toPromise(); 
       default:
         return this.http.get('http://localhost:3000/cliente_all').toPromise();
     }
