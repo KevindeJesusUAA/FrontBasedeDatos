abb1fda4db66ed5af17bdaa54052bf0da41d9653
AlanUzielo <lalanuziel@gmail.com>
2023-11-16 22:20:40 -0600

    Validaciones en los campos de altas

--- src/app/altas/altas.component.ts
+++ src/app/altas/altas.component.ts
@@ -26,101 +26,142 @@ export class AltasComponent {
 
   constructor(private router: Router, private servicio: DbclientService){}
 
-  cargarTabla(){
-    //this.router.navigate([this.tabla]);
-  }
-
   altaCliente(){
-    const cliente = {
-      NombreCliente: this.nombre,
-      EdadCliente: this.edad,
-      TelefonoCliente: this.telefono,
-      DireccionCliente: this.direccion
-    };
+    if(this.validaTelefono() && this.validaNombre()){
+      const cliente = {
+        NombreCliente: this.nombre,
+        EdadCliente: this.edad,
+        TelefonoCliente: this.telefono,
+        DireccionCliente: this.direccion
+      };
 
-    this.servicio.altaCliente(cliente).subscribe(
-      (response:any)=>{
-        console.log(response);
-      },
-      (error:any)=>{
-        console.log(error);
-      }
-    );
+      this.servicio.altaCliente(cliente).subscribe(
+        (response:any)=>{
+          console.log(response);
+        },
+        (error:any)=>{
+          console.log(error);
+        }
+      );
+      
+    }else
+      console.log("NO ENTRA");
   }
 
   altaEmpleado(){
-    const empleado = {
-      NombreEmpleado: this.nombre,
-      TelefonoEmpleado: this.telefono,
-      DireccionEmpleado: this.direccion,
-      EdadEmpleado: this.edad,
-      SueldoEmpleado: this.sueldo
-    };
+    if(this.validaNombre() && this.validaTelefono()){
+      const empleado = {
+        NombreEmpleado: this.nombre,
+        TelefonoEmpleado: this.telefono,
+        DireccionEmpleado: this.direccion,
+        EdadEmpleado: this.edad,
+        SueldoEmpleado: this.sueldo
+      };
 
-    this.servicio.altaEmpleado(empleado).subscribe(
-      (response:any)=>{
-        console.log(response);
-      },
-      (error:any)=>{
-        console.log(error);
-      }
-    );
+      this.servicio.altaEmpleado(empleado).subscribe(
+        (response:any)=>{
+          console.log(response);
+        },
+        (error:any)=>{
+          console.log(error);
+        }
+      );
+    }else 
+      console.log("No ENTRA");
   }
 
   altaProveedor(){
-    const proveedor = {
-      NombreProveedor: this.nombre,
-      EdadProveedor: this.edad,
-      TelefonoProveedor: this.telefono,
-      DireccionProveedor: this.direccion
-    };
+    if(this.validaNombre() && this.validaTelefono()){
+      const proveedor = {
+        NombreProveedor: this.nombre,
+        EdadProveedor: this.edad,
+        TelefonoProveedor: this.telefono,
+        DireccionProveedor: this.direccion
+      };
 
-    this.servicio.altaProveedor(proveedor).subscribe(
-      (response:any)=>{
-        console.log(response);
-      },
-      (error:any)=>{
-        console.log(error);
-      }
-    );
+      this.servicio.altaProveedor(proveedor).subscribe(
+        (response:any)=>{
+          console.log(response);
+        },
+        (error:any)=>{
+          console.log(error);
+        }
+      );
+    }else
+      console.log("No entra");
   }
 
   altaDoctor(){
-    const doctor = {
-      NombreDoctor: this.nombre,
-      EdadDoctor: this.edad,
-      TelefonoDoctor: this.telefono,
-      DireccionDoctor: this.direccion,
-      TurnoDoctor: this.turno,
-      SueldoDoctor: this.sueldo
-    };
+    if(this.validaNombre() && this.validaTelefono() &&
+      this.validaTurno()){
+      const doctor = {
+        NombreDoctor: this.nombre,
+        EdadDoctor: this.edad,
+        TelefonoDoctor: this.telefono,
+        DireccionDoctor: this.direccion,
+        TurnoDoctor: this.turno,
+        SueldoDoctor: this.sueldo
+      };
 
-    this.servicio.altaDoctor(doctor).subscribe(
-      (response:any)=>{
-        console.log(response);
-      },
-      (error:any)=>{
-        console.log(error);
-      }
-    );
+      this.servicio.altaDoctor(doctor).subscribe(
+        (response:any)=>{
+          console.log(response);
+        },
+        (error:any)=>{
+          console.log(error);
+        }
+      );
+    }else 
+      console.log("No Entra");
   }
 
   altaProducto(){
-    const producto = {
-      NombreProducto: this.nombre,
-      MarcaProducto: this.marca,
-      CostoProducto: this.costo,
-      ExistenciaProducto: this.existencia
-    };
+    if(this.validaNombre() && this.validaMarca()){
+      const producto = {
+        NombreProducto: this.nombre,
+        MarcaProducto: this.marca,
+        CostoProducto: this.costo,
+        ExistenciaProducto: this.existencia
+      };
 
-    this.servicio.altaProducto(producto).subscribe(
-      (response:any)=>{
-        console.log(response);
-      },
-      (error:any)=>{
-        console.log(error);
-      }
-    );
+      this.servicio.altaProducto(producto).subscribe(
+        (response:any)=>{
+          console.log(response);
+        },
+        (error:any)=>{
+          console.log(error);
+        }
+      );
+    }else 
+      console.log("No entra");
   }  
 
+
+  validaTelefono():boolean{
+    if(/^[0-9]*$/.test(this.telefono))
+      return true;
+    else
+      return false;
+  }
+
+  validaNombre():boolean{
+    if(/^[a-zA-Z\s]*$/.test(this.nombre))
+      return true;
+    else
+      return false;
+  }
+
+  validaMarca():boolean{
+    if(/^[a-zA-Z\s]*$/.test(this.marca))
+      return true;
+    else
+      return false;
+  }
+
+  validaTurno():boolean{
+    if(/^(matutino|vespertino)$/i.test(this.turno))
+      return true;
+    else
+      return false;
+  }
 }
